fa169ce0a219a97e10a47db216c7d6b36a296bf0
omar <omarisel01@gmail.com>
2013-02-16 09:07:45 -0600

    Configurando rutas estaticas, y render para la vista inicio

---
+++ apps/inicio/views.py
@@ -0,0 +1,17 @@
+ #encoding:utf-8
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+ 
+def inicio_view(request):
+ 
+ 
+	ctx = {
+
+			'inicio_active' : True ,
+
+		  }
+	
+	return render_to_response('inicio/inicio.html',ctx,  context_instance = RequestContext(request)  )
+
+
--- tributario/urls.py
+++ tributario/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
+#Registro de URLS
+ 
 urlpatterns = patterns('',
+
+	url(r'^' , include('apps.inicio.urls')) , 
+
     # Examples:
     # url(r'^$', 'tributario.views.home', name='home'),
     # url(r'^tributario/', include('tributario.foo.urls')),
@@ -15,3 +21,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
