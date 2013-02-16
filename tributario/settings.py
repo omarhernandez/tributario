1f6026fc32ffd70a7c963f411af24e61cfaf974e
omar <omarisel01@gmail.com>
2013-02-16 10:18:35 -0600

    agregando middleware Auth

--- tributario/settings.py
+++ tributario/settings.py
@@ -97,7 +97,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -117,16 +119,17 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
 
+        'django.contrib.sessions',
+
         'django.contrib.auth',
-        'django.contrib.contenttypes',
-        #'django.contrib.sessions',
-        #'#django.contrib.sites',
-        #'django.contrib.messages',
+ 
         'django.contrib.staticfiles',
-        # Uncomment the next line to enable the admin:
-        # 'django.contrib.admin',
-        # Uncomment the next line to enable admin documentation:
-        # 'django.contrib.admindocs',
+
+        'apps.inicio',
+
+        'apps.login',
+ 
+ 
         )
 
 # A sample logging configuration. The only tangible logging
