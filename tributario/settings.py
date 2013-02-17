cc5507b053d75ff27e084f4fb89f00c5307b4fc1
omar <omarisel01@gmail.com>
2013-02-16 23:47:31 -0600

    recibo

--- tributario/settings.py
+++ tributario/settings.py
@@ -16,10 +16,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'trib_db',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'nf9ckpg',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -164,5 +164,3 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
  
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://root@localhost/trib_db')}
